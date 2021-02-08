a5579215e1d28e1026a2e3065b63bbadaa22cf06
david.collins <david.collins@cvent.com>
2021-02-08 09:34:38 -0500

    update hogan version

--- Formula/hogan.rb
+++ Formula/hogan.rb
@@ -1,7 +1,7 @@
 class Hogan < Formula
   desc "Customized handlebars wrapper, with config inheritance"
   homepage "https://github.com/cvent/hogan"
-  url "https://github.com/cvent/hogan/releases/download/v0.2.10/hogan-v0.11.2-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/cvent/hogan/releases/download/v0.11.2/hogan-v0.11.2-x86_64-apple-darwin.tar.gz"
   sha256 "ed83bdf39a1b987ac83dde475d3f15c8cb23493af7301454b94aa90337054048"
   revision 1
 
