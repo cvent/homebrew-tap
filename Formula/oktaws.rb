baf7eec5eba2b65ec33deb24a5a668080bfdad6c
Chris Pernicano <cpernicano@cvent.com>
2022-03-09 14:08:55 -0500

    update oktaws to 0.15.5

--- Formula/oktaws.rb
+++ Formula/oktaws.rb
@@ -1,9 +1,9 @@
 class Oktaws < Formula
   desc 'CLI access to AWS with Okta Credentials'
-  version "0.15.1"
+  version "0.15.5"
   homepage 'https://github.com/jonathanmorley/oktaws'
   url "https://github.com/jonathanmorley/oktaws/releases/download/v#{version}/oktaws-x86_64-apple-darwin.tar.gz"
-  sha256 '396b960ea258558218403e6c58982d09603a0752c005dd22bbbef9bce6d1f28e'
+  sha256 'd585a7812262c173151b40d4f21ea25e254cb0bfabed3f445a5a092f0d974c0e'
   revision 1
 
   def install
